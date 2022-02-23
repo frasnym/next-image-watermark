95f0d99823df79131c2f312fb97e2f500ae7af47
FrasNym <frastyawan.nym@gmail.com>
2022-02-23 11:11:11 +0800

    Update meta and favicon

--- components/layouts/default-layout.tsx
+++ components/layouts/default-layout.tsx
@@ -25,31 +25,31 @@ const PageContainer = styled.div`
 const DefaultLayout: NextPage<DefaultLayoutProps> = function ({ children, title }) {
   return (
     <Container>
-      {/* <Head>
-        <title>{title} | FrasNym</title>
+      <Head>
+        <title>{title}WOI by FrasNym</title>
         <link rel="shortcut icon" href="/favicon.ico" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
 
-        <meta name="title" content="Nyoman Frastyawan (FrasNym) - Software Engineer" />
-        <meta name="description" content="Indonesian 🇮🇩 Backend Developer. At your service with Golang and Javascript." />
-        <meta name="keywords" content="nyoman frastyawan, frasnym, frasnym portfolio, frasnym developer, frasnym freelance" />
+        <meta name="title" content="Watermart On Image by FrasNym" />
+        <meta name="description" content="Create your custom watermark on image without worrying about your data." />
+        <meta name="keywords" content="watermark, watermark image" />
 
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://frasnym.tech/" />
-        <meta property="og:title" content="Nyoman Frastyawan (FrasNym) - Software Engineer" />
-        <meta property="og:description" content="Indonesian 🇮🇩 Backend Developer. At your service with Golang and Javascript." />
-        <meta property="og:image" content="https://raw.githubusercontent.com/frasnym/next-portfolio-v2/main/public/images/avatar.jpg" />
+        <meta property="og:title" content="Watermart On Image by FrasNym" />
+        <meta property="og:description" content="Create your custom watermark on image without worriying about your data." />
+        <meta property="og:image" content="https://i.ibb.co/F5pSJB3/WOI.png" />
         <meta property="og:locale" content="en_ID" />
         <meta property="og:locale:alternate" content="en_ID" />
 
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://frasnym.tech/" />
-        <meta property="twitter:title" content="Nyoman Frastyawan (FrasNym) - Software Engineer" />
-        <meta property="twitter:description" content="Indonesian 🇮🇩 Backend Developer. At your service with Golang and Javascript." />
-        <meta property="twitter:image" content="https://raw.githubusercontent.com/frasnym/next-portfolio-v2/main/public/images/avatar.jpg" />
+        <meta property="twitter:title" content="Watermart On Image by FrasNym" />
+        <meta property="twitter:description" content="Create your custom watermark on image without worriying about your data." />
+        <meta property="twitter:image" content="https://i.ibb.co/F5pSJB3/WOI.png" />
         <meta name="twitter:site" content="@frasnym" />
         <meta name="twitter:creator" content="@frasnym" />
-      </Head> */}
+      </Head>
       <NavBar />
       <PageContainer>
         {children}
