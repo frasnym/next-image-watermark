8ac0516fd9080df2b24fa23194207a2ac19bfc29
FrasNym <frastyawan.nym@gmail.com>
2022-03-14 11:14:49 +0800

    refactor: Move HomeForm component to its own folder

---
+++ components/home-page/HomeForm/HomeForm.tsx
@@ -0,0 +1,59 @@
+import type { NextPage } from "next";
+import { ImageSize } from "../../../types";
+import FileInput from "../../input/FileInput";
+import TextAreaInput from "../../input/TextAreaInput";
+import Button from "../../ui/Button";
+import {
+  FormBody,
+  FormContainer,
+  FormFooter,
+  FormGroup,
+} from "./HomeForm.style";
+
+interface HomeFormProps {
+  imageName: string;
+  imageDownloadUrl?: string;
+  onImageChangeHandler: (file: File) => void;
+  watermarkText: string;
+  onWatermarkChangeHandler: (text: string) => void;
+  imageSize: ImageSize;
+  onImageSizeChangeHandler: (imgSize: ImageSize) => void;
+}
+
+const HomeForm: NextPage<HomeFormProps> = function (props) {
+  return (
+    <FormContainer>
+      <FormBody>
+        <FormGroup>
+          <label>Image File</label>
+          <FileInput onChangeHandler={props.onImageChangeHandler} />
+        </FormGroup>
+        <FormGroup>
+          <label>Watermark Text</label>
+          <TextAreaInput
+            onChangeHandler={(text: string) =>
+              props.onWatermarkChangeHandler(text)
+            }
+            value={props.watermarkText}
+          />
+        </FormGroup>
+      </FormBody>
+      <FormFooter>
+        <hr />
+        <Button
+          text="Download"
+          onClickHandler={function () {
+            if (!props.imageDownloadUrl) return;
+
+            const link = document.createElement("a");
+            link.download = props.imageName;
+            link.href = props.imageDownloadUrl;
+            link.click();
+          }}
+        />
+      </FormFooter>
+    </FormContainer>
+  );
+};
+
+export default HomeForm;
---
+++ types/index.d.ts
@@ -0,0 +1,4 @@
+export type ImageSize = {
+  height: number
+  width: number,
+}
